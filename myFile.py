e196eee1d65deb7a574c0d292bfed162acb35e7d
Souschef1984 <5069277+Souschef1984@users.noreply.github.com>
2019-04-20 20:15:43 -0700

    Submittin Assignment

---
+++ myFile.py
@@ -0,0 +1,6 @@
+# myFile.py
+
+def bereavment():
+    print("Hello Bill. It has not been easy to catch up on the assignments, as they were mostly locked until sometime last night or this morning. Apologies for coming up missing, but the family has not taken well to my father passing away.")
+
+bereavment()
